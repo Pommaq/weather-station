ff2537aaaaa3ccaa4080f87dae96decbb2a019aa
Timothy Hjort <timothy.hjort@outlook.com>
2023-05-04 00:31:52 +0200

    added some navtex documentation

--- external-services/src/navtex.rs
+++ external-services/src/navtex.rs
@@ -1,5 +1,13 @@
 /// https://goughlui.com/2019/10/05/project-navtex-message-gallery-august-september-2019/
 
+/*
+All NAVTEX broadcasts are made on 518 kHz, using narrow-band direct printing 7-unit forward
+ error correcting (FEC or Mode B) transmission.
+ This type of transmission is also used by Amateur Radio service (AMTOR).
+ Broadcasts use 100 baud FSK modulation, with a frequency shift of 170 Hz.
+ The center frequency of the audio spectrum applied to a single sideband transmitter is 1700 Hz.
+ The receiver 6 dB bandwidth should be between 270-340 Hz.
+*/
 #[cfg(test)]
 mod tests {
     #[test]
