3830c460dcaf79824fd31c4d61ba955e4dd3c3a6
Timothy Hjort <timothy.hjort@outlook.com>
2023-05-04 00:24:28 +0200

    formatted project

--- persistence/src/websdr.rs
+++ persistence/src/websdr.rs
@@ -1,5 +1,3 @@
-/// https://goughlui.com/2019/10/05/project-navtex-message-gallery-august-september-2019/
-
 #[cfg(test)]
 mod tests {
     #[test]
--- services/src/lib.rs
+++ services/src/lib.rs
@@ -1,5 +1,5 @@
-extern crate persistence;
 extern crate external_services;
+extern crate persistence;
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
