173566ea360bd65b331dace8429f5522b18f6cd0
Timothy Hjort <timothy@outlook.com>
2023-05-17 19:57:29 +0200

    First iteration of drawing graphs

--- services/src/lib.rs
+++ services/src/lib.rs
@@ -1,27 +1,32 @@
 extern crate external_services;
 extern crate persistence;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use anyhow::{Ok, Result};
+use hound;
 
-pub mod iq {
-    use external_services::navtex::{iq, wav};
-    use anyhow::Result;
+pub fn get_amplitudes_from_wav(path: &str) -> Result<Vec<f64>> {
+    use external_services::iq::AmplitudeIterator;
 
-    fn get_amplitude_iterator(path: &str) -> impl Iterator<Item = f64> {
-            
-        todo!()
-    }
+    let mut reader = hound::WavReader::open(path).unwrap();
+    let binding: Vec<std::result::Result<i16, hound::Error>> =
+        reader.samples::<i16>().collect::<Vec<_>>();
+
+    let mut iterator = binding.windows(2).step_by(2).map(|x| {
+        let i = x[0].as_ref().unwrap();
+        let q = x[1].as_ref().unwrap();
+        (i, q)
+    });
+    Ok(AmplitudeIterator::new(&mut iterator).collect())
 }
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
+    const SAMPLEPATH: &str = "../samples/navtex_2023-02-21T16_40_30_201 mono.wav";
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn amplitude_iter_test() {
+        let _ = get_amplitudes_from_wav(SAMPLEPATH).unwrap();
     }
 }
